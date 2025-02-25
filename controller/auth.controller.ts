c36eed4c112f4fd7df381b01a683863e90b920f3
Souhardya20 <souhardyadeb2016@gmail.com>
2025-02-25 10:27:50 +0530

    test

--- controller/auth.controller.ts
+++ controller/auth.controller.ts
@@ -66,7 +66,7 @@ export const adminLogin = asyncHandler(async (req: Request, res: Response) => {
 			httpOnly: true,
 			secure: true, 
 			sameSite: "none",
-			domain: ".jgecalumni.in",
+			domain: "admin.jgecalumni.in",
 		});
 
 		response.status(200).json({
