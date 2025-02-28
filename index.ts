dad9deee1e25241ff95f73dbd6c4c7b4aa34ba8a
Souhardya20 <souhardyadeb2016@gmail.com>
2025-02-28 20:59:50 +0530

    fixed

--- index.ts
+++ index.ts
@@ -18,8 +18,8 @@ import authentication from "./middleware/authentication";
 
 // configure middlewares
 app.use(cookieParser());
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(
 	cors({
 		origin: [
